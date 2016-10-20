b8c92aca869d2dee2c3eb56c8f134def18a2c027
Nicholas Rohrer <nrohrer2@gmail.com>
2016-10-20 15:36:54 -0500

    changing APIs due to CORS

--- main.js
+++ main.js
@@ -1,32 +1,45 @@
-$( document ).ready(function() {
+$( document ).ready();
 
-    // cache the DOM
-    var $myCity = $('#city');
-    var $myState = $('#state');
-    var $myTemp = $('#temp');
-    var $myWeather = $('#weather');
+// cache the DOM
+var $myCity = $('#city');
+var $myState = $('#state');
+var $myTemp = $('#temp');
+var $myWeather = $('#weather');
 
-    // variables to display
-    var theTemp = 0;
-    var theCity = "";
-    var theState = "";
-    var theWeather = "";
-    var lat;
-    var long;
+// variables to display
+var theTemp = 0;
+var theCity = "";
+var theState = "";
+var theWeather = "";
+var lat = 0;
+var lon = 0;
 
-    // get user's city, state, and lat and long
-    $.getJSON('http://ipinfo.io', function(data){
-	    console.log(data);
-	    theCity = data.city;
-	    theState = data.region;
-	    locArray = data.loc.split(",");
-	    lat = locArray[0]; 
-	    long = locArray[1];
-	    console.log(lat);
-	    console.log(long);
-	})
+// get user's city, state, and lat and long
+function getLoc() {
+    $.getJSON('http://ip-api.com/json/?callback=?', function(data){
+    console.log(data);
+    theCity = data.city;
+    theState = data.regionName;
+    lat = data.lat; 
+    lon = data.lon;
+    console.log(lat);
+    console.log(lon);
 
-    $.getJSON('https://api.darksky.net/forecast/ee7f8c5319292e0f2d3c2a15f2e0327e/' + lat + "," + long, function(data){
-    	console.log(data);
+    //$.getJSON('https://api.darksky.net/forecast/ee7f8c5319292e0f2d3c2a15f2e0327e/' + lat + "," + lon + "/callback=?", function(data){
+        //console.log(data);
+    //})
+
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=8a10021e7f41b82c85ebfa59a6ecabd5&callback=?', function(data){
+        console.log(data);
     })
-});
+        
+    })
+    
+//function getWeather() {
+    //$.getJSON('https://api.darksky.net/forecast/ee7f8c5319292e0f2d3c2a15f2e0327e/' + lat + "," + long, function(data){
+        //console.log(data);
+    //})
+}
+
+getLoc();
+//getWeather();
