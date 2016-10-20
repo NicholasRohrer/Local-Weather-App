9c5655f8d290def85e2774e50c7f71ab4e3ddf9d
NICHOLAS ROHRER <nrohrer2@NICHOLASs-MacBook-Pro.local>
2016-10-20 18:46:12 -0500

    APIs working, displays user location now

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var $myState = $('#state');
 var $myTemp = $('#temp');
 var $myWeather = $('#weather');
 
-// variables to display
+// variables
 var theTemp = 0;
 var theCity = "";
 var theState = "";
@@ -17,28 +17,28 @@ var lon = 0;
 // get user's city, state, and lat and long
 function getLoc() {
     $.getJSON('http://ip-api.com/json/?callback=?', function(data){
-    console.log(data);
-    theCity = data.city;
-    theState = data.regionName;
-    lat = data.lat; 
-    lon = data.lon;
-    console.log(lat);
-    console.log(lon);
 
-    //$.getJSON('https://api.darksky.net/forecast/ee7f8c5319292e0f2d3c2a15f2e0327e/' + lat + "," + lon + "/callback=?", function(data){
-        //console.log(data);
-    //})
+	    // display data for testing
+	    console.log(data);
 
-    $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=8a10021e7f41b82c85ebfa59a6ecabd5&callback=?', function(data){
-        console.log(data);
-    })
+	    // grab user coordinates
+	    lat = data.lat; 
+	    lon = data.lon;
+	    console.log(lat);
+	    console.log(lon);
+
+	    //populate the city and state of the user
+	    $myCity.html(data.city);
+	    $myState.html(data.regionName);
+
+
+	    // grab weather for user's location
+	    $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=8a10021e7f41b82c85ebfa59a6ecabd5&callback=?', function(data){
+	        // display data for testing
+	        console.log(data);
+	    })
         
     })
-    
-//function getWeather() {
-    //$.getJSON('https://api.darksky.net/forecast/ee7f8c5319292e0f2d3c2a15f2e0327e/' + lat + "," + long, function(data){
-        //console.log(data);
-    //})
 }
 
 getLoc();
