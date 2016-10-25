b8202be51bb86627b70256b8924497bf9e54249f
NICHOLAS ROHRER <nrohrer2@NICHOLASs-MacBook-Pro.local>
2016-10-25 18:30:20 -0500

    cleaned up code, need to add weather icons

--- main.js
+++ main.js
@@ -5,13 +5,12 @@ var $myCity = $('#city');
 var $myState = $('#state');
 var $myTemp = $('#temp');
 var $myWeather = $('#weather');
+var $degFButton = $('#degF');
+var $degCButton = $('#degC');
 
 // variables
 var degF = 0;
 var degC = 0;
-var theCity = "";
-var theState = "";
-var theWeather = "";
 var lat = 0;
 var lon = 0;
 
@@ -20,45 +19,38 @@ var unit = 0;
 
 // get user's city, state, and lat and long
 function getLoc() {
-    $.getJSON('http://ip-api.com/json/?callback=?', function(data){
-
-	    // display data for testing
-	    console.log(data);
+    $.getJSON('http://ip-api.com/json/?callback=?', function(data1){
 
 	    // grab user coordinates
-	    lat = data.lat; 
-	    lon = data.lon;
-	    console.log(lat);
-	    console.log(lon);
-
-	    //populate the city and state of the user
-	    $myCity.html(data.city);
-	    $myState.html(data.regionName);
+	    lat = data1.lat; 
+	    lon = data1.lon;
 
 	    // grab weather for user's location
-	    $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=8a10021e7f41b82c85ebfa59a6ecabd5&callback=?', function(data){
+	    $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=8a10021e7f41b82c85ebfa59a6ecabd5&callback=?', function(data2){
 
 	        // display data for testing
-	        console.log(data);
+	       	console.log(data2);
 
 	        // convert Kelvin to degrees F
-	        degF = Math.ceil(data.main['temp'] * (9/5) - 459.67);
+	        degF = Math.ceil(data2.main['temp'] * (9/5) - 459.67);
 	        // convert Kelvin to degrees C
-	        degC = Math.ceil(data.main['temp'] - 273.15)
+	        degC = Math.ceil(data2.main['temp'] - 273.15)
 
+	        $myCity.html(data1.city + ", ");
+	    	$myState.html(data1.regionName);
 	        // display weather description and degF by default for user
 	        $myTemp.html(degF + ' &degF');
-	        $myWeather.html(data.weather[0].description);
+	        $myWeather.html(data2.weather[0].description);
 
 	        // change units on click of button
-	        $('#degC').on('click', function(){
+	        $degCButton.on('click', function(){
 	        	if (unit === 0) {
 	        		unit = 1;
 	        		$myTemp.html(degC + ' &degC');
 	        	}
 	        });
 
-	        $('#degF').on('click', function(){
+	        $degFButton.on('click', function(){
 	        	if (unit === 1) {
 	        		unit = 0;
 	        		$myTemp.html(degF + ' &degF');
@@ -67,18 +59,6 @@ function getLoc() {
 
         })
     })
-}
-
-function toCelsius(){
-
-	var x = document.getElementById("temp").value;
-	console.log(x);
-	// check the units
-	//if (unit === 1) {
-		//return;
-	//} 
-
-
 }
 
 // run the APIs when the page loads
