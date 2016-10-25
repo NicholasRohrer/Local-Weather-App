a6ba001d2b7be46db8dfd0ea822c89290bc9f407
NICHOLAS ROHRER <nrohrer2@NICHOLASs-MacBook-Pro.local>
2016-10-25 16:20:37 -0500

    added unit change functionality

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ var $myTemp = $('#temp');
 var $myWeather = $('#weather');
 
 // variables
-var theTemp = 0;
+var degF = 0;
+var degC = 0;
 var theCity = "";
 var theState = "";
 var theWeather = "";
@@ -34,29 +35,51 @@ function getLoc() {
 	    $myCity.html(data.city);
 	    $myState.html(data.regionName);
 
-
 	    // grab weather for user's location
 	    $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=8a10021e7f41b82c85ebfa59a6ecabd5&callback=?', function(data){
 
 	        // display data for testing
 	        console.log(data);
 
-	        var theTemp = Math.ceil(data.main['temp'] * (9/5) - 459.67);
-	        // grab user temp and weather description
-	        $myTemp.html(theTemp + ' &degF');
+	        // convert Kelvin to degrees F
+	        degF = Math.ceil(data.main['temp'] * (9/5) - 459.67);
+	        // convert Kelvin to degrees C
+	        degC = Math.ceil(data.main['temp'] - 273.15)
+
+	        // display weather description and degF by default for user
+	        $myTemp.html(degF + ' &degF');
 	        $myWeather.html(data.weather[0].description);
-	   
+
+	        // change units on click of button
+	        $('#degC').on('click', function(){
+	        	if (unit === 0) {
+	        		unit = 1;
+	        		$myTemp.html(degC + ' &degC');
+	        	}
+	        });
+
+	        $('#degF').on('click', function(){
+	        	if (unit === 1) {
+	        		unit = 0;
+	        		$myTemp.html(degF + ' &degF');
+	        	}
+	        });
+
         })
     })
 }
 
 function toCelsius(){
+
+	var x = document.getElementById("temp").value;
+	console.log(x);
 	// check the units
-	if (unit === 1) {
-		return;
-	} 
+	//if (unit === 1) {
+		//return;
+	//} 
 
 
 }
 
+// run the APIs when the page loads
 getLoc();
