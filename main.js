66e06bb68dcf6f7a18d38235039ffef0ee2227ed
Nicholas Rohrer <nrohrer2@gmail.com>
2016-11-24 14:58:36 -0600

    using new API for user location

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var $degCButton = $('#degC');
 var $weatherIcon = $('#weatherIcon');
 
 // variables
-var degF = 0;
+var degF = 0;	
 var degC = 0;
 var lat = 0;
 var lon = 0;
@@ -19,8 +19,29 @@ var iconCode = 0;
 // if unit = 0, unit is degrees F. unit = 1 = degrees C
 var unit = 0;
 
+// get user lat and long
+var loc = document.getElementById('myloc');
+
+function myLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(getCoords);
+	}
+	else {
+		loc.innerHTML = "Location Tracking not Possible";
+	}
+}
+
+function getCoords(position) {
+	lat = position.coords.longitude;
+	lon = position.coords.latitude;
+	console.log(lat);
+	console.log(lon);
+}
+
+
+
 // get user's city, state, and lat and long
-function getLoc() {
+/*function getLoc() {
     $.getJSON('http://ip-api.com/json/?callback=?', function(data1){
 
 	    // grab user coordinates
@@ -67,3 +88,20 @@ function getLoc() {
 
 // run the APIs when the page loads
 getLoc();
+
+var loc = document.getElementById('myloc');
+
+function myLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	}
+	else {
+		loc.innerHTML = "Location Tracking not Possible";
+	}
+}
+
+function showPosition(position) {
+	loc.innerHTML = "Longitude: " + position.coords.longitude;
+}
+
+*/
