90c80337ced4a8003304a7880fc46ce1c30dce06
NICHOLAS ROHRER <nrohrer2@gmail.com>
2016-12-09 17:31:14 -0600

    cleaned up whitespace in js file

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ var $hideWeather = $('#hideWeather');
 var $fadeThis = $('.fadeThis');
 var $getWeatherBtn = $('#getWeatherBtn');
 
-
 // variables
 var degF = 0;	
 var degC = 0;
@@ -31,9 +30,7 @@ var lat = 0;
 var lon = 0;
 var iconCode = 0;
 const KEY = 'ee7f8c5319292e0f2d3c2a15f2e0327e';
-
-// if unit = 0, unit is degrees F. unit = 1 = degrees C
-var unit = 0;
+var unit = 0; // if unit = 0, unit is degrees F. unit = 1 = degrees C
 
 
 function convertToCelsius(temp) {
@@ -105,54 +102,3 @@ $.getJSON('https://geoip-db.com/json/geoip.php?jsonp=?', function(location) {
 
 // display weather on click of getWeatherBtn
 $getWeatherBtn.on('click', toggleWeather);
-
-// get user's city, state, and lat and long
-/*function getLoc() {
-    $.getJSON('http://ip-api.com/json/?callback=?', function(data1){
-
-	    // grab user coordinates
-	    lat = data1.lat; 
-	    lon = data1.lon;
-
-	    // grab weather for user's location
-	    $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=8a10021e7f41b82c85ebfa59a6ecabd5&callback=?', function(data2){
-
-	        // convert Kelvin to degrees F
-	        degF = Math.ceil(data2.main['temp'] * (9/5) - 459.67);
-
-	        // convert Kelvin to degrees C
-	        degC = Math.ceil(data2.main['temp'] - 273.15);
-
-	        // get weather icon code
-	        iconCode = data2.weather[0].icon;
-
-	        // display city, state, temp, weather, and icon to page
-	        $myCity.html(data1.city + ", ");
-	    	$myState.html(data1.regionName);
-	        $myTemp.html(degF + ' &degF');
-	        $myWeather.html(data2.weather[0].description);
-	        $weatherIcon.attr("src", "http://openweathermap.org/img/w/" + iconCode + ".png");
-
-	        // change units on click of the buttons
-	        $degCButton.on('click', function(){
-	        	if (unit === 0) {
-	        		unit = 1;
-	        		$myTemp.html(degC + ' &degC');
-	        	}
-	        });
-
-	        $degFButton.on('click', function(){
-	        	if (unit === 1) {
-	        		unit = 0;
-	        		$myTemp.html(degF + ' &degF');
-	        	}
-	        });
-
-        })
-    })
-}
-
-// run the APIs when the page loads
-getLoc();
-
-*/
